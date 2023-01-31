789ef4af167b0af926abb804393d464af5e50f22
ryutarokanazawa <srtxtrader@gmail.com>
2023-01-31 20:33:05 +0900

    再度Vercelように変更

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ window.addEventListener('scroll', function () {
 
     } else {
         // if (activePath === "/index.html") {
-        if(document.URL.match(/index/)){
+        if(document.URL.match(/vercel/)){
             fixed.style.position = "absolute";
         } else {
             fixed.style.position = "initial";
@@ -71,7 +71,7 @@ const jsText = ".js-mv_title-item span";
 const jsHeader = ".js-header";
 
 // if (activePath == "index.html") {
-if(document.URL.match(/index/)){
+if(document.URL.match(/vercel/)){
 
     /* 文字列を分割しspanで囲む */
     (function () {
